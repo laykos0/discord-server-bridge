b78a886008dcef2bf89c9fe61f8366743fcdc1b7
Atrate <Atrate@protonmail.com>
2020-10-29 17:22:22 +0100

    [Repo] Add copyright notice
    Signed-off-by: Atrate <Atrate@protonmail.com>

--- discordForwarder.py
+++ discordForwarder.py
@@ -1,3 +1,14 @@
+# Copyright (C) 2020 laykos0 <laykos0@protonmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation; version 3 of the License.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
 import discord
 
 # Insert bot token here:
