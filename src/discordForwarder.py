27bfdae0877bd9bb53471351420461e247f59f8b
laykos0 <laykos0@protonmail.com>
2021-02-23 12:12:40 +0100

    [discordForwarder.py] Image fix

--- src/discordForwarder.py
+++ src/discordForwarder.py
@@ -16,19 +16,25 @@ token = ""
 client = discord.Client()
 
 
-async def forward(message, destination):
+async def forward(*args):
+    message = args[0]
+    destination = args[1]
     for guild in client.guilds:
         if guild != message.guild:
             for channel in guild.channels:
                 if channel.name in destination:
                     embed = discord.Embed()
                     embed.set_author(name=message.author, icon_url=message.author.avatar_url)
+                    embed.set_footer(text="Created at: " + str(message.created_at).split(".")[0])
                     embed.add_field(name="Server:", value=str(message.guild), inline=False)
-                     if len(message.content) > 1023:
+                    if len(message.content) > 1023 and message.content:
                         embed.add_field(name="Message part 1/2", value=message.content[0: 1023], inline=True)
-                        embed.add_field(name="Message part 2/2", value=message.content[1023: len(message.content)], inline=True)
-                    else:
+                        embed.add_field(name="Message part 2/2", value=message.content[1023: len(message.content)],
+                                        inline=True)
+                    elif message.content:
                         embed.add_field(name="Message:", value=message.content, inline=False)
+                    if len(args) == 3:
+                        embed.set_image(url=args[2])
                     await channel.send(content=None, embed=embed)
 
 
@@ -41,7 +47,10 @@ async def on_message(message):
     # Insert your own bot tag here:
     bot_tag = ""
 
-    if str(message.channel) in channels and str(message.author) != bot_tag:
+    if message.attachments and str(message.channel) in channels and str(message.author) != bot_tag:
+        url = str(message.attachments[0]).split("url='")[1].split("'")[0]
+        await forward(message, destination, url)
+    elif str(message.channel) in channels and str(message.author) != bot_tag:
         await forward(message, destination)
 
 
